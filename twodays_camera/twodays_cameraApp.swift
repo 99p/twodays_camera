62586365e57bbe24132d62b2f8f42ac74ebb304c
99p <poccori@gmail.com>
2023-02-10 15:11:13 +0900

    Initial Commit

---
+++ twodays_camera/twodays_cameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  twodays_cameraApp.swift
+//  twodays_camera
+//
+//  Created by macboy on 2023/02/10.
+//
+
+import SwiftUI
+
+@main
+struct twodays_cameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
